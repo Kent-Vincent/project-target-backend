c5c7cd72284fa51f0516681717faa886ed2269e4
Kent-Vincent <sarsalejo02@gmail.com>
2024-04-21 19:59:09 +0800

    working login but only username

--- target/accounts/views.py
+++ target/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
@@ -18,10 +19,8 @@ class Accounts(APIView):
         return Response(content)
     
 class CustomAuthToken(ObtainAuthToken):
-
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
+        serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
