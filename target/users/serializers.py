e9ee6447758d92f808e2d0397943910e796a89ec
Kent-Vincent <sarsalejo02@gmail.com>
2024-04-25 09:09:48 +0800

    fixed password issue but now its not a superuser

--- target/users/serializers.py
+++ target/users/serializers.py
@@ -1,8 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True) 
 
     class Meta:
         model =  User
@@ -12,3 +13,11 @@ class UserSerializer(ModelSerializer):
             'first_name',
             'last_name',
         )
+
+    def create(self, validated_data):
+        # Extract password from validated data and hash it before saving
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
