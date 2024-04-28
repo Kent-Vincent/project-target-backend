2d9afa4e1119ec1974dd2f49338e02ec959c5381
Kent-Vincent <sarsalejo02@gmail.com>
2024-04-29 07:23:48 +0800

    altered table and fixed some typos on table

--- target/tickets/models.py
+++ target/tickets/models.py
@@ -9,19 +9,16 @@ class Tickets(models.Model):
         ('MEDIUM', 'Medium'),
         ('HIGH', 'High'),
     ]
-    # typo
-    ticketName = models.CharField(max_length=100)  
+    ticket_name = models.CharField(max_length=100)  
     description = models.TextField()
-    # attachments = models.FileField(null=True, upload_to='attachments/')
-    attachments = models.TextField(null=True, blank=True)
+    attachments = models.FileField(null=True, upload_to='attachments/')
     time_elapsed = models.DurationField(null=True, blank=True)
     avatar_icon = models.ImageField(upload_to='avatars/', null=True, blank=True)
     assignee = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
     due_date = models.DateField()
     priority = models.CharField(max_length=10, choices=TICKET_PRIORITIES, default='LOW')
-    # fix typo
-    fileBy_avatar_icon = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    filed_by_avatar_icon = models.ImageField(upload_to='avatars/', null=True, blank=True)
     filed_by = models.CharField(max_length=100)
     cover_photo = models.ImageField(upload_to='covers/', null=True, blank=True)
 
