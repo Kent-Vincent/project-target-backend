3bd14b340a61bb4cfbe71c19f27f56ca533511db
Jonelbars <jonel.barrientos@hcdc.edu.ph>
2024-05-02 16:35:43 +0800

    boards & tickets changes

--- target/tickets/urls.py
+++ target/tickets/urls.py
@@ -1,8 +1,8 @@
 # myapp/urls.py
 from django.urls import path
-from .views import TicketsCreate
+from .views import TicketsCreate, TicketDisplay
 
 urlpatterns = [
     path('create/', TicketsCreate.as_view(), name='ticket-create'),
-    path('display/', TicketsCreate.as_view(), name='ticket-display'),
+    path('display/', TicketDisplay.as_view(), name='ticket-display'),
 ]
