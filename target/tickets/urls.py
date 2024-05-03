edd5ad7916533c3d7959cf86ce2f088b3c2a8cab
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-03 09:58:49 +0800

    working admin side assigning workspace and creating

--- target/tickets/urls.py
+++ target/tickets/urls.py
@@ -4,5 +4,5 @@ from .views import TicketsCreate, TicketDisplay
 
 urlpatterns = [
     path('create/', TicketsCreate.as_view(), name='ticket-create'),
-    path('display/', TicketDisplay.as_view(), name='ticket-display'),
+    # path('display/', TicketDisplay.as_view(), name='ticket-display'),
 ]
