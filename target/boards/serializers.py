29be5cbf2ca22ac39b7d8158cd89d8d683faa582
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-03 09:59:05 +0800

    removed boards

--- target/boards/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Workspace
-
-from rest_framework import serializers
-
-class BoardsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workspace
-        fields = '__all__'
--- target/boards/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BoardsDisplay, BoardsCreate
-
-urlpatterns = [
-    path('create/', BoardsCreate.as_view(), name='boards-create'),
-    path('display/', BoardsDisplay.as_view(), name='boards-display'),
-]
