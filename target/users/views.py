ceb16d8c269ca64020774b16a3e3abfbed632ca9
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-06 22:23:22 +0800

    working create account will request a token

--- target/users/views.py
+++ target/users/views.py
@@ -33,8 +33,9 @@ class RegisterUserView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class CurrentUserView(APIView):
--- target/workspace/urls.py
+++ target/workspace/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import current_workspace, current_stage
 
 urlpatterns = [
-    path('current/workspace', current_workspace, name='current_workspace'),
-    path('current/stage', current_stage, name='current_stage')
+    path('current/workspace/', current_workspace, name='current_workspace'),
+    path('current/stage/', current_stage, name='current_stage')
 ]
