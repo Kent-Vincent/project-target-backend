83e77103f4f55a2a74f257494fe8eadffcc5b6b1
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-06 09:51:46 +0800

    added stages view and url

--- target/workspace/urls.py
+++ target/workspace/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import current_workspace
+from .views import current_workspace, current_stage
 
 urlpatterns = [
-    path('current/', current_workspace, name='current_workspace'),
+    path('current/workspace', current_workspace, name='current_workspace'),
+    path('current/stage', current_stage, name='current_stage')
 ]
--- target/workspace/views.py
+++ target/workspace/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Workspace
-from workspace.serializers import WorkspaceSerializer
+from .models import Workspace, Stage
+from workspace.serializers import WorkspaceSerializer, StageSerializer
 
 @api_view(['GET'])
 def current_workspace(request):
@@ -18,3 +18,27 @@ def current_workspace(request):
             return Response({'error': 'Workspace does not exist'}, status=status.HTTP_404_NOT_FOUND)
     else:
         return Response({'error': 'User is not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+@api_view(['GET'])
+def current_stage(request):
+    if request.user.is_authenticated:
+        try:
+            workspace = Workspace.objects.filter(users=request.user).first()
+            if workspace:
+                # Filter the stage based on the workspace
+                stage = Stage.objects.filter(workspace=workspace).all()
+                if stage:
+                    serializer = StageSerializer(stage, many=True)
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response({'error': 'Stage not found for this workspace'}, status=status.HTTP_404_NOT_FOUND)
+            else:
+                return Response({'error': 'Workspace not found for this user'}, status=status.HTTP_404_NOT_FOUND)
+        except Stage.DoesNotExist:
+            return Response({'error': 'Stage does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response({'error': 'User is not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    
+
+  
