f46688773a1429b793b4ecadf7ea84dbf435816f
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-16 07:10:43 +0800

    added API for creating ticket not finished

--- target/tickets/urls.py
+++ target/tickets/urls.py
@@ -4,5 +4,4 @@ from .views import TicketsCreate, TicketDisplay
 
 urlpatterns = [
     path('create/', TicketsCreate.as_view(), name='ticket-create'),
-    # path('display/', TicketDisplay.as_view(), name='ticket-display'),
 ]
